4e79d886a6d365b21b9c6dd53476bf6f382ba800
Rob Ranf <rob@emiyaconsulting.com>
2023-06-14 12:11:56 -0700

    Initial Commit

---
+++ BaseballStats/BaseballStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaseballStatsApp.swift
+//  BaseballStats
+//
+//  Created by Rob Ranf on 6/14/23.
+//
+
+import SwiftUI
+
+@main
+struct BaseballStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
